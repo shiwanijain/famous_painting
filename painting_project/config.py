2c88f5786382b78bb9a090c278fe180a19447004
adityachoudhury29 <achoudhury2004@gmail.com>
2025-03-31 18:19:58 +0530

    [EDIT] Updated CSS

--- painting_project/config.py
+++ painting_project/config.py
@@ -1,6 +1,6 @@
 class Config:
     MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "Youcan@123"
+    MYSQL_USER = "aditya"
+    MYSQL_PASSWORD = "Aditya04@mysql"
     MYSQL_DB = "painting_db"
     # SECRET_KEY = "your_secret_key"S
